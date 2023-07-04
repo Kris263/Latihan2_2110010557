cb7343c545d100582166fa3ea8cb3668776ef0aa
Kris263 <kristianptr2633@gmail.com>
2023-07-04 21:01:41 +0800

    Membuat class Dosen dan DosenBeraksi

---
+++ src/kampus/Dosen.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kampus;
+
+/**
+ *
+ * @author krist
+ */
+public class Dosen {
+    String nidn, nama, mataKuliah;
+    
+    public String getDetail() {
+        return nidn + nama + mataKuliah;
+    }
+    
+    
+}
